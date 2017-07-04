28655695b3d9995a6ede7673897b6fa581b32aca
Anastasia Nomerovskaya <nomerovskaya.a@hardpoint.eu>
2017-07-04 15:32:12 +0300

    Add project files.

---
+++ TrainingOfSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingOfSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = sizeof(int);
+            int a = sizeof(bool);
+
+            Console.WriteLine("Размерность типа int=" + i);
+            Console.WriteLine("Размерность типа bool=" + a);
+            Console.Read();
+        }
+    }
+}
